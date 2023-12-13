50187195f44de90a8b4fcf4acf727cca57d2749e
Klimpavlov <Klimpavlov2002@gmail.com>
2023-12-13 21:59:40 +0300

    organized a space for a hiClass development

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Header from "@/components/Header/Header";
+
+export default
+function ExplorePage() {
+  return (
+    <main className="">
+      <Header/>
+    </main>
+  )
+}
