e6d1ea60dee0508859b31b445589e5a8ed17b396
Klimpavlov <Klimpavlov2002@gmail.com>
2024-07-25 09:46:46 +0300

    isVerified translations

--- src/app/[locale]/signUp/verifyEmail/page.js
+++ src/app/[locale]/signUp/verifyEmail/page.js
@@ -7,6 +7,8 @@ import ContinueButton from "@/components/Buttons/ContinueButton";
 import postVerificationCode from "@/app/[locale]/signUp/verifyEmail/postVerificationCode/postVerificationCode";
 import axios from "axios";
 import {useRouter} from "next/navigation";
+import {useTranslations} from "next-intl";
+
 
 export default function verifyEmail() {
     const router = useRouter();
@@ -18,6 +20,7 @@ export default function verifyEmail() {
         router.push('/signUp/verifyEmail/isVerified')
     }
 
+    const t = useTranslations("SignUp.VerifyEmail")
 
     return (
         <main>
@@ -25,23 +28,22 @@ export default function verifyEmail() {
             <div className='flex flex-col items-center justify-center'>
                 <div className="content flex flex-col items-center gap-8 w-full
              max-w-screen-sm p-4 md:p-8 lg:p-16 xl:p-20 2xl:p-32">
-                    <div className="text-4xl whitespace-pre-line">Verify your email address</div>
-                    <div className=" ">We’ve sent a verification link to <span className='user-email'>{email}</span>
+                    <div className="text-4xl whitespace-pre-line text-center">{t("verifyFormText")}</div>
+                    <div className="text-center">{t("sendLinkTo")}<span className='user-email'>{email}</span>
                     </div>
-                    <div className=''>Click the link to complete the verification process. If you don’t see it you may
-                        need to check your spam folder.
+                    <div className=''>{t("clickLink")}
                     </div>
                     <div className="divider"></div>
                     <div className="inputs w-full ">
                         <div className="my-4">
-                            <InputForm inputFormText="Verification code" placeholderText="Enter verification code"
+                            <InputForm inputFormText={t("verificationCode")} placeholderText={t("placeholderVerificationCode")}
                                        value={code}
                                        onChange={(e) => setCode(e.target.value)}
 
                             />
                         </div>
                     </div>
-                    <ContinueButton buttonText="Continue" onClick={handleContinue}/>
+                    <ContinueButton buttonText={t("ContinueBtn")} onClick={handleContinue}/>
                 </div>
             </div>
         </main>
