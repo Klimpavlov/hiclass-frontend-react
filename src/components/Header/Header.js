9c9271251864bfe9a2824ebc9d413b5e24530b7e
Klimpavlov <Klimpavlov2002@gmail.com>
2024-10-02 23:35:36 +0300

    fix: notification bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -126,7 +126,6 @@ const Header = ({testNotifications}) => {
     async function getNotifications() {
         const accessToken = sessionStorage.getItem('accessToken');
         const notificationsFromApi = await getAllNotifications(accessToken);
-        console.log(notificationsFromApi[0].message.split(' ')[0]);
         setReceivedNotifications(notificationsFromApi.map((notification) => ({
             notificationId: notification.notificationId,
             message: notification.message.split(' ')[0] + ' ' + t("invitationMessage"),
