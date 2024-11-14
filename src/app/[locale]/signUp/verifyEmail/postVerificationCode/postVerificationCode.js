a3c8bbb8b4fda8d730f608ee7cb1b7463bd6c5d0
Klimpavlov <Klimpavlov2002@gmail.com>
2024-11-14 23:57:45 +0300

    fix: postVerifCode bug

--- src/app/[locale]/signUp/verifyEmail/postVerificationCode/postVerificationCode.js
+++ src/app/[locale]/signUp/verifyEmail/postVerificationCode/postVerificationCode.js
@@ -1,41 +1,3 @@
-// import axios from "axios";
-// import getLocalhost from "@/app/[locale]/api/localhost/localhost";
-// import apiClient from "@/app/[locale]/api/utils/axios";
-//
-// const postVerificationCode = async (email, code) => {
-//     const accessToken = localStorage.getItem('accessToken')
-//     const localhost = getLocalhost();
-//     const deviceToken = localStorage.getItem('deviceToken')
-//
-//     const url = `http://${localhost}/api/User/verify-email`;
-//     const requestUrl = `${url}?verificationCode=${code}`;
-//
-//     await apiClient.post(`http://${localhost}/api/User/verify-email`, {
-//         DeviceToken: deviceToken,
-//         Email: email,
-//         VerificationCode: code,
-//     }, {
-//         headers: {
-//             Authorization: `Bearer ${accessToken}`,
-//         }
-//     })
-//         .then(function (response) {
-//             console.log(response);
-//             console.log(response.data.value)
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//             // Перенаправление на страницу с ошибкой при ошибке запроса
-//         });
-// }
-//
-// export default postVerificationCode
-//
-//
-//
-
-
-import axios from "axios";
 import getLocalhost from "@/app/[locale]/api/localhost/localhost";
 import apiClient from "@/app/[locale]/api/utils/axios";
 import Cookies from "js-cookie";
@@ -80,5 +42,42 @@ const postVerificationCode = async (email, code, successRedirect, toast, t) => {
 
 export default postVerificationCode
 
+
+
+// import axios from "axios";
+// import getLocalhost from "@/app/[locale]/api/localhost/localhost";
+// import apiClient from "@/app/[locale]/api/utils/axios";
+//
+// const postVerificationCode = async (email, code) => {
+//     const accessToken = localStorage.getItem('accessToken')
+//     const localhost = getLocalhost();
+//     const deviceToken = localStorage.getItem('deviceToken')
+//
+//     const url = `http://${localhost}/api/User/verify-email`;
+//     const requestUrl = `${url}?verificationCode=${code}`;
+//
+//     await apiClient.post(`http://${localhost}/api/User/verify-email`, {
+//         DeviceToken: deviceToken,
+//         Email: email,
+//         VerificationCode: code,
+//     }, {
+//         headers: {
+//             Authorization: `Bearer ${accessToken}`,
+//         }
+//     })
+//         .then(function (response) {
+//             console.log(response);
+//             console.log(response.data.value)
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//             // Перенаправление на страницу с ошибкой при ошибке запроса
+//         });
+// }
+//
+// export default postVerificationCode
+//
+//
+//
 
 
