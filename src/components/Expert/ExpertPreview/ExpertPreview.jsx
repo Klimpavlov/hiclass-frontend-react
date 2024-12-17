8b373e0b0b5999dbf5b6cb84532251614a7ebc28
Klimpavlov <Klimpavlov2002@gmail.com>
2024-12-17 21:18:22 +0300

    fix: tag isExpert

--- src/components/Expert/ExpertPreview/ExpertPreview.jsx
+++ src/components/Expert/ExpertPreview/ExpertPreview.jsx
@@ -6,10 +6,13 @@ import imgDefaultClass from "@/components/Class/ClassPreview/defaultClassImage.j
 import defaultUserImage from "@/components/User/UserInfo/avatar-default.svg";
 import Tag from "@/components/Tags/Tag";
 import {useTranslations} from "next-intl";
+import imgLightning from "@/components/User/UserInfo/lightning.svg";
 
 const ExpertPreview = ({photo, userAvatar, username, tags, grades}) => {
 
     const gradeTranslation = useTranslations("Grade");
+    const t = useTranslations("IsExpert");
+
 
     return (
         <div className='w-64'>
@@ -24,6 +27,7 @@ const ExpertPreview = ({photo, userAvatar, username, tags, grades}) => {
 
                 </div>
                 <div className='class-preview-header flex items-center mt-1'>
+                    <Image src={imgLightning} alt={imgLightning}/>
                     <div className='relative w-8 h-8 rounded-full overflow-hidden'>
                         <Image
                             src={userAvatar || defaultUserImage}
@@ -35,8 +39,15 @@ const ExpertPreview = ({photo, userAvatar, username, tags, grades}) => {
                     </div>
                     <div className="username text-black font-bold cursor-pointer ml-1">{username}</div>
                 </div>
+                {/*<div className='flex'>*/}
+                {/*    <div className='flex border rounded-xl border-cyan-600 py-1 px-2 my-6 w-auto'>*/}
+                {/*        <Image src={imgLightning} alt={imgLightning}/>*/}
+                {/*        <div className='ml-2 text-sky-800'>{t('expert')}</div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
                 <div className='flex justify-between mt-1'>
                     <div className="flex flex-wrap gap-2">
+                        <Tag text={t('expert')}/>
                         {tags.map((title) => (
                             <Tag key={title} text={title}></Tag>
                         ))}
