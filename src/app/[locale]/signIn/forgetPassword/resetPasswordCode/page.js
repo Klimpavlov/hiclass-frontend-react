c5dd316db3b4aa71413fca9b639cc89d9f019c01
Klimpavlov <Klimpavlov2002@gmail.com>
2025-03-17 11:32:46 +0300

    fix: translate notification

--- src/app/[locale]/signIn/forgetPassword/resetPasswordCode/page.js
+++ src/app/[locale]/signIn/forgetPassword/resetPasswordCode/page.js
@@ -47,9 +47,13 @@ export default function resetPasswordCode() {
                 <div className="content flex flex-col items-center gap-8 w-full
              max-w-screen-sm p-4 md:p-8 lg:p-16 xl:p-20 2xl:p-32">
                     <div className="text-4xl whitespace-pre-line text-center">{t("resetPasswordCode")}</div>
-                    <div className="text-center">{t("verificationLinkText")}<span className='user-email'>{resetPasswordEmail}</span>
-                    </div>
-                    <div className='text-center'>{t("clickLinkText")}
+                    {/*<div className="text-center">{t("verificationLinkText")}<span className='user-email'>{resetPasswordEmail}</span>*/}
+                    {/*</div>*/}
+                    {/*<div className='text-center'>{t("clickLinkText")}*/}
+                    {/*</div>*/}
+                    <div className="text-center">{t("verificationLinkText")}
+                        <span className='user-email'>{resetPasswordEmail}. </span>
+                        <span className="text-center">{t("clickLinkText")}</span>
                     </div>
                     <div className="divider"></div>
                     <div className="inputs w-full ">
--- src/app/[locale]/signUp/verifyEmail/page.js
+++ src/app/[locale]/signUp/verifyEmail/page.js
@@ -45,7 +45,9 @@ export default function verifyEmail() {
                 <div className="content flex flex-col items-center gap-8 w-full
              max-w-screen-sm p-4 md:p-8 lg:p-16 xl:p-20 2xl:p-32">
                     <div className="text-4xl whitespace-pre-line text-center">{t("verifyFormText")}</div>
-                    <div className="text-center">{t("sendLinkTo")}<span className='user-email'>{email}</span>
+                    <div className="text-center">{t("sendLinkTo")}
+                        <span className='user-email'>{email}. </span>
+                        <span className="text-center">{t("checkCode")}</span>
                     </div>
                     <div className="divider"></div>
                     <div className="inputs w-full ">
