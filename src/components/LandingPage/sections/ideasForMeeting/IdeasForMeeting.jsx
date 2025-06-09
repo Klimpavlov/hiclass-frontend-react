59aeaa9f2952c4723a02e2ec1af53472473bea87
frontbuffed <frontend1@buffedsoft.com>
2025-06-09 18:59:36 +0300

    fix: uninstall unnecessary library

--- src/components/LandingPage/sections/ideasForMeeting/IdeasForMeeting.jsx
+++ src/components/LandingPage/sections/ideasForMeeting/IdeasForMeeting.jsx
@@ -11,6 +11,16 @@ const IdeasForMeeting = () => {
                     Творческие учителя - творческие уроки!
                 </p>
             </div>
+            <div className="max-w-[800px]">
+                <h2 className="text-white text-[36px] font-normal text-center">
+                    "Teaching with Hi, Class is a joy. The platform enhance the learning experience for both me and my students. It's a place where my passion for teaching truly thrives."
+                </h2>
+            </div>
+            <div className="flex flex-col items-center text-center gap-2 max-w-[720px]">
+                <Image />
+                <span className='text-[16px] font-semibold'>Liza Ermakova</span>
+                <span>Teacher</span>
+            </div>
         </>
     );
 };
